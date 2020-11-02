b230d0e1acca5e8d1a05eb15a8f9422337969776
MusabMahmoodh <musabmah70707@gmail.com>
2020-11-02 18:01:43 +0530

    Initial setup and folder structure

---
+++ server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+import {getPosts , createPost} from '../controllers/posts.js'
+const route = express.Router()
+
+
+route.get("/",getPosts)
+route.post("/",createPost)
+
+export default route;
